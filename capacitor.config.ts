923109026ecd18bd4fb01ea6f750fd8a09788336
Mohamme542-a <rqyqmhmd28@gmail.com>
2026-08-24 10:58:35 +0100

    feat: add sanitized AB User Android source

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.qarfash.abuser",
   appName: "AB",
-  webDir: "../dist/public",
+  webDir: "dist/public",
 };
 
 export default config;
